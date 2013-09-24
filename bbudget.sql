6b1a58620fce2b665ae2c5b70dada3ae77aaba2f
Noel <noelesp@gmail.com>
2013-09-24 17:42:57 -0400

    Update bbudget.sql

--- bbudget.sql
+++ bbudget.sql
@@ -18,8 +18,8 @@ SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 --
 -- Database: `bbudget`
 --
-CREATE DATABASE `bbudget_exp` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `bbudget_exp`;
+CREATE DATABASE `bbudget` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+USE `bbudget`;
 
 -- --------------------------------------------------------
 
